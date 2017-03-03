09efede2388a945a888876fcbf1508d9464c2cf6
Yang Ming <d112233445563@gmail.com>
2017-03-03 13:01:00 +0800

    command if else ok

--- ass1/hw1_105753032.R
+++ ass1/hw1_105753032.R
@@ -2,17 +2,24 @@
 # homework1 example
 ########################
 
-# system("Rscript hw1_105753032.R -files test.1.csv –out result.csv")
+# system("Rscript hw1_105753032.R -files test.1.csv -out result2.csv")
 
 args = commandArgs(trailingOnly=TRUE)
-
 if (length(args)==0) {
   stop("USAGE: Rscript hw1_exam.R input", call.=FALSE)
-} else if (length(args) > 1) {
-  i_f <- args[2] 
+} else {
+  for(i in c(1:4) ){
+    if (args[i] == "-out"){
+      out <- args[ i+1 ]
+    } else if (args[i] == "-files"){
+      i_f <- args[ i+1 ]
+    }
+  } 
 }
 
-i_f <- "test.1.csv"
+
+
+### i_f <- "test.1.csv"
 data <- read.csv(i_f)
 
 w <- round(max(data$weight), digits = 2)
@@ -23,4 +30,4 @@ y <- c(i_f, w, h )
 
 final <- rbind(x, y)
 
-write.table(final, file = "test2.CSV", sep = ",", row.names = FALSE, col.names = FALSE)
+write.table(final, file = out , sep = ",", row.names = FALSE, col.names = FALSE)
