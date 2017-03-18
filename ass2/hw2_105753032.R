3ec3697df78133dc1a234c9e65c12b625ec07185
Yang Ming <d112233445563@gmail.com>
2017-03-18 20:49:35 +0800

    ass2 done

--- ass2/hw2_105753032.R
+++ ass2/hw2_105753032.R
@@ -3,7 +3,7 @@
 query_func<-function(query_m, i)
 {
   if(query_m == "male"){
-    which.min(i)
+    which.max(i)
   }
   else if (query_m == "female") {
     which.max(i)
@@ -11,6 +11,7 @@ query_func<-function(query_m, i)
     stop(paste("ERROR: unknown query function", query_m))
   }
 }
+
 
 # read parameters
 args = commandArgs(trailingOnly=TRUE)
@@ -44,43 +45,60 @@ print(paste("output file:", out_f))
 print(paste("files      :", files))
 
 
-
-
-
-
 # read files
 names<-c()
-weis<-c()
-heis<-c()
+sens<-c()
+spes<-c()
+F1s<-c()
+AUCs<-c()
+
+#files <- "method1.csv"
+#query_m <- "male"
+
 for(file in files)
 {
   name<-gsub(".csv", "", basename(file))
   d<-read.table(file, header=T,sep=",")
-  weis<-c(weis, d$weight[query_func(query_m, d$weight)])
-  heis<-c(heis, d$height[query_func(query_m, d$height)])
+  
+  if(query_m == "male"){
+    cM <- table(truth=d$reference, prediction=d$prediction)
+    sensitivity <- round ( cM[2, 2] / (cM[2, 1] + cM[2, 2]), digits = 2)
+    specificity <- round ( cM[1, 1] / (cM[1, 1] + cM[1, 2]), digits = 2)
+    precision <- round ( cM[2, 2] / (cM[1, 2] + cM[2, 2]), digits = 2)
+    F1 <- round ( 2 *  precision * sensitivity / (precision + sensitivity), digits = 2)
+  }
+  else if (query_m == "female") {
+    cM <- table(truth=d$reference, prediction=d$prediction)
+    sensitivity <- round (cM[1, 1] / (cM[1, 2] + cM[1, 1]), digits = 2)
+    specificity <- round (cM[2, 2] / (cM[2, 1] + cM[2, 2]), digits = 2)
+    precision <- round (cM[1, 1] / (cM[1, 1] + cM[2, 1]), digits = 2)
+    F1 <- round (2 *  precision * sensitivity / (precision + sensitivity), digits = 2)
+  } else {
+    stop(paste("ERROR: unknown query function", query_m))
+  }
+  
+# AUC evaluation
+  library('ROCR')
+  eval <- prediction(d$pred.score,d$reference)
+  AUC <- round(attributes(performance(eval,'auc'))$y.values[[1]], digits = 2)
+  
+  sens<-c(sens, sensitivity)
+  spes<-c(spes, specificity)
+  F1s<-c(F1s, F1)
+  AUCs<-c(AUCs, AUC)
   names<-c(names,name)
 }
 
-library(ROCR);
-d <- read.table("method1.csv", header = T, sep = ",")
-y <- d$reference
-pred <- d$prediction
-
-cM <- table(truth=d$reference, prediction=d$prediction)
-print(cM)
-
-pred <- prediction(predictions, y);
-
-# Specificity = the  true negative rate TN/(TN+FP)
 # Sensitivity = the  true positive rate = recall = TP/(TP+FN)
+# Specificity = the  true negative rate TN/(TN+FP)
 # precision = TP/(TP+FP)
 # F1 = 2*precision*recall/(precision+recall)
 # AUC
 
 
-#out_data<-data.frame(set=names, wei=weis, hei=heis, stringsAsFactors = F)
-#index<-sapply(out_data[,c("wei","hei")], query_func, query_m=query_m)
+out_data<-data.frame(method=names, sensitivity=sens, specificity=spes, F1=F1s, AUC=AUCs, stringsAsFactors = F)
+index<-sapply(out_data[,c("sensitivity","specificity","F1","AUC")], query_func, query_m=query_m)
 
 # output file
-#out_data<-rbind(out_data,c(query_m,names[index]))
-#write.table(out_data, file=out_f, row.names = F, quote = F)
+out_data<-rbind(out_data,c("highest",names[index]))
+write.table(out_data, file=out_f, row.names = F, quote = F)
